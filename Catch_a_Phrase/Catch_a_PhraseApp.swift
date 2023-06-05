e8b71ddeff1975980752266b26ba441ad2dd2183
yuncoffee <dong072815@gmail.com>
2023-06-05 17:24:41 +0900

    Initial Commit

---
+++ Catch_a_Phrase/Catch_a_PhraseApp.swift
@@ -0,0 +1,20 @@
+//
+//  Catch_a_PhraseApp.swift
+//  Catch_a_Phrase
+//
+//  Created by Yun Dongbeom on 2023/05/22.
+//
+
+import SwiftUI
+
+@main
+struct Catch_a_PhraseApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ Catch_a_PhraseUITests/Catch_a_PhraseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Catch_a_PhraseUITestsLaunchTests.swift
+//  Catch_a_PhraseUITests
+//
+//  Created by Yun Dongbeom on 2023/05/22.
+//
+
+import XCTest
+
+final class Catch_a_PhraseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
