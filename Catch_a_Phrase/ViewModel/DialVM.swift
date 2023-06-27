f0282e68503e5d79d5d98f6c9af564d22610d6ce
yuncoffee <dong072815@gmail.com>
2023-06-28 00:22:02 +0900

    feat: DialVM 옵저버 프로퍼티 추가

--- Catch_a_Phrase/ViewModel/DialVM.swift
+++ Catch_a_Phrase/ViewModel/DialVM.swift
@@ -6,8 +6,110 @@
 //
 
 import Foundation
+import AVFoundation
 
 class DialVM: ObservableObject {
+    private let DIAL_LIMIT_ANGLE: CGFloat = 32
+    private let KR_CHARS_MAX_LIST = [18, 20, 27] // 초성, 중성, 종성
     
+    @Published
+    var isShowAnimation = false{
+        didSet {
+            if isShowAnimation {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 1.6) {
+                    self.isShowAnimation = false
+                }
+            }
+        }
+    }
+    
+    /// 다이얼이 돌아간 값을 저장하기 위한 값
+    @Published
+    var totalRotates = [CGSize.zero, CGSize.zero, CGSize.zero] {
+        didSet {
+            // 왼쪽 다이얼
+            if prevRotates[0].height + DIAL_LIMIT_ANGLE < totalRotates[0].height {
+                prevRotates[0] = totalRotates[0]
+                krScalers[0] += 1
+            }
+            if prevRotates[0].height - DIAL_LIMIT_ANGLE > totalRotates[0].height {
+                prevRotates[0] = totalRotates[0]
+                krScalers[0] -= 1
+            }
+            // 오른쪽 다이얼
+            if prevRotates[1].height + DIAL_LIMIT_ANGLE < totalRotates[1].height {
+                prevRotates[1] = totalRotates[1]
+                krScalers[1] += 1
+            }
+            if prevRotates[1].height - DIAL_LIMIT_ANGLE > totalRotates[1].height {
+                prevRotates[1] = totalRotates[1]
+                krScalers[1] -= 1
+            }
+            // 아래쪽 다이얼
+            if prevRotates[2].width + DIAL_LIMIT_ANGLE < totalRotates[2].width {
+                prevRotates[2] = totalRotates[2]
+                krScalers[2] -= 1
+            }
+            if prevRotates[2].width - DIAL_LIMIT_ANGLE > totalRotates[2].width {
+                prevRotates[2] = totalRotates[2]
+                krScalers[2] += 1
+            }
+        }
+    }
+    
+    @Published
+    var prevRotates = [CGSize.zero, CGSize.zero, CGSize.zero] {
+        didSet {
+            HapticManager.shared.impact(type: .rigid)
+            AudioServicesPlaySystemSoundWithCompletion(1157, nil);
+        }
+    }
+    
+    @Published
+    var currentRotates = [CGSize.zero, CGSize.zero, CGSize.zero]
+    
+    /// 초성: 18자,
+    /// 중성: 20자,
+    /// 종성: 27자
+    @Published
+    var krScalers = [0, 0, 0] {
+        didSet {
+            if krScalers[0] < 0 {
+                krScalers[0] = KR_CHARS_MAX_LIST[0]
+            } else if krScalers[0] > KR_CHARS_MAX_LIST[0] {
+                krScalers[0] = 0
+            }
+            if krScalers[1] < 0 {
+                krScalers[1] = KR_CHARS_MAX_LIST[1]
+            } else if krScalers[1] > KR_CHARS_MAX_LIST[1] {
+                krScalers[1] = 0
+            }
+            if krScalers[2] < 0 {
+                krScalers[2] = KR_CHARS_MAX_LIST[2]
+            } else if krScalers[2] > KR_CHARS_MAX_LIST[2] {
+                krScalers[2] = 0
+            }
+        }
+    }
 }
 
+extension DialVM {
+//    func updateCurrentCharcter(globalStore: GlobalStore) {
+//        if let firstScaler = Unicode.Scalar(0x1100 + krScalers[0]),
+//           let secondaryScaler = Unicode.Scalar(0x1161 + krScalers[1]),
+//           let thirdScaler = Unicode.Scalar(0x11a6 + 1 + krScalers[2])
+//        {
+//            let char = krScalers[2] == 0
+//            ? String(firstScaler).appending(String(secondaryScaler))
+//            : String(firstScaler).appending(String(secondaryScaler)).appending(String(thirdScaler))
+//
+//            globalStore.currentCharcter = char
+//
+//            if globalStore.currentCharcter.count > 0 && globalStore.correctYetWord.count > 0 &&   globalStore.currentCharcter[globalStore.currentCharcter.startIndex] == globalStore.correctYetWord[globalStore.correctYetWord.startIndex] {
+//                isShowAnimation = true
+//            } else {
+//                isShowAnimation = false
+//            }
+//        }
+//    }
+}
